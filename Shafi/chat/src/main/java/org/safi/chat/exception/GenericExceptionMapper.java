fd44e0908910be314dd96922e448e97d820ba782
safi3989 <safiizz24@gmail.com>
2017-06-18 19:17:28 +0530

    solution.txt

--- Shafi/chat/src/main/java/org/safi/chat/exception/GenericExceptionMapper.java
+++ Shafi/chat/src/main/java/org/safi/chat/exception/GenericExceptionMapper.java
@@ -14,7 +14,7 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
 	@Override
 	public Response toResponse(Throwable ex) {
-		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500, "http://javabrains.koushik.org");
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500, "http://www.google.com");
 		return Response.status(Status.INTERNAL_SERVER_ERROR)
 				.entity(errorMessage)
 				.build();
